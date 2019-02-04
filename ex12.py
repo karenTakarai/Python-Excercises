06bc8a34e7133d5ce7b5e26947051a5fd71f5d9f
karentakarai <karen.torresgmz@gmail.com>
2019-02-04 02:42:16 -0600

    exercise 12 added

---
+++ ex12.py
@@ -0,0 +1,9 @@
+#prompt a question and wait for an answer of the user, then put the answer on the age variable
+age = int(input("How old are you? "))
+#prompt a question and wait for an answer of the user, then put the answer on the height variable
+height = input("how tall are you? ")
+#propmpt a question and wait for answer of the user, then put the answer on the weight variable
+weight = input("How much do you weigh? ")
+
+#show the answers of the user
+print(f"So, you're {age} old, {height} tall and {weight} heavy.")
